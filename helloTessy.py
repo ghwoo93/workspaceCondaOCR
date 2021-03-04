d86705a92ce06f6110c2370c988176c09d3cb101
ghwoo93 <wkungh93@gmail.com>
2021-03-04 19:47:42 +0900

    dir change

--- helloTessy.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image       #pip install pillow
-from pytesseract import *   #pip install pytesseract
-import configparser
-import os
-
-#Config Parser 초기화
-config = configparser.ConfigParser()
-#Config File 읽기
